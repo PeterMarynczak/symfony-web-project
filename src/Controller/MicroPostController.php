979288ff324c30276eda2ccd598db03150196f44
Piotr Marynczak <piotr.marynczak@wiosna.org.pl>
2022-12-08 13:56:30 +0100

    getting posts from database

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -13,15 +13,16 @@ class MicroPostController extends AbstractController
     #[Route('/micro-post', name: 'app_micro_post')]
     public function index(MicroPostRepository $posts): Response
     {
-        $microPost = new MicroPost();
-        $microPost->setTitle('Controller');
-        $microPost->setText('It came from controller');
-        $microPost->setCreated(new \DateTime());
-
-        $posts->save($microPost, true);
-
         return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
+            'posts' => $posts->findAll(),
+        ]);
+    }
+
+    #[Route('/micro-post/{post}', 'app_micro_post_show')]
+    public function showOne(MicroPost $post): Response
+    {
+        return $this->render('micro_post/show.html.twig', [
+            'post' => $post,
         ]);
     }
 }
