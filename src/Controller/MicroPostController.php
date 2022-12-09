12f357bd4f9008c9e72308bc35337f6419f45dc5
Piotr Marynczak <piotr.marynczak@wiosna.org.pl>
2022-12-09 17:18:41 +0100

    added form to create and edit posts

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\MicroPost;
+use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,5 +27,55 @@ class MicroPostController extends AbstractController
         return $this->render('micro_post/show.html.twig', [
             'post' => $post,
         ]);
+    }
+
+    #[Route('/micro-post/add-post', 'app_micro_post_add_post', priority: 2)]
+    public function addPost(Request $request, MicroPostRepository $posts): Response
+    {
+        $form = $this->createForm(MicroPostType::class, new MicroPost());
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $post = $form->getData();
+            $post->setCreated(new \DateTime());
+            $posts->save($post, true);
+
+            $this->addFlash('success', 'Your micropost has been added');
+
+            return $this->redirectToRoute('app_micro_post');
+        }
+
+        return $this->renderForm(
+            'micro_post/add.html.twig',
+            [
+            'form' => $form,
+            ]);
+    }
+
+
+    #[Route('/micro-post/{post}/edit-post', 'app_micro_post_edit_post')]
+    public function editPost(MicroPost $post, Request $request, MicroPostRepository $posts): Response
+    {
+        $form = $this->createForm(MicroPostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $post = $form->getData();
+            $posts->save($post, true);
+
+            $this->addFlash('success', 'Your micropost has been updated');
+
+            return $this->redirectToRoute('app_micro_post');
+        }
+
+        return $this->renderForm(
+            'micro_post/add.html.twig',
+            [
+                'form' => $form,
+            ]);
     }
 }
